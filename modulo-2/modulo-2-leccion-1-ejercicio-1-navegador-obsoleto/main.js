2b8507a5b50f20ead56fe41a6399879c2f7e5ae5
mZubeldia <mzubeldia09@gmail.com>
2021-04-29 20:49:57 +0200

    my first js code

---
+++ modulo-2/modulo-2-leccion-1-ejercicio-1-navegador-obsoleto/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+document.querySelector('h1').innerHTML = 'Esta página no es compatible con la versión actual de tu navegador. Por favor, actualízalo a la versión más reciente.';
+
+var myAddres = 'Calle Castilla';
+myAddres = 'Calle Pureza';
