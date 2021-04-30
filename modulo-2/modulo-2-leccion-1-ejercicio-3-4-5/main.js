050097e0f41d215ddc4b0d24f28c2d9246b614ef
mZubeldia <mzubeldia09@gmail.com>
2021-04-30 18:03:00 +0200

    first js exercises

---
+++ modulo-2/modulo-2-leccion-1-ejercicio-3-4-5/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//const paragraphElement = document.querySelector('.text');
+//paragraphElement.innerHTML = 'Hola mundo';
+
+const mainTitle = document.querySelector(".js-title");
+//mainTitle.innerHTML = "La adalaber seleccionada es María";
+//mainTitle.innerHTML = "La adalaber seleccionada es Lorena"; 
+//mainTitle.classList.add("js-item1");
+
+mainTitle.innerHTML += ' Lorena';
+
+const password = document.querySelector(".js-password");
+password.innerHTML = "Mi contraseña es **";
