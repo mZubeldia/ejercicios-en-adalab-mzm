017da85ad3b64bcd888aa1981669c8edbec18252
mZubeldia <mzubeldia09@gmail.com>
2021-05-03 16:00:29 +0200

    exercise 6, adding strings

---
+++ modulo-2/modulo-2-leccion-2-ejercicio-6-hola-adalaber-strings/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+const adalaberName = " Lola";
+const greeting = "Hola,";
+const polite = ", encantada de conocerte."
+const fullGreeting = greeting + adalaberName + polite;
+
+console.log(fullGreeting);
