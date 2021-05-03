9b41463a5d609bf25a9694b9189daca52b226e6b
mZubeldia <mzubeldia09@gmail.com>
2021-05-03 16:08:13 +0200

    exercise 7, interpolando

---
+++ modulo-2/modulo-2-leccion-2-ejercicio-7-interpola-lola/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+const adalaberName = " Lola";
+const greeting = "Hola";
+//const polite = ", encantada de conocerte."
+//const fullGreeting = greeting + adalaberName + polite;
+
+//console.log(fullGreeting);
+
+console.log(`${greeting}${adalaberName}, encantada de conocerte.`);
+
