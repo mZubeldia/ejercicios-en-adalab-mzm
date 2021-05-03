51c8b55834663698cb64e05f10ac6ea0d4e7a9d9
mZubeldia <mzubeldia09@gmail.com>
2021-05-03 11:17:55 +0200

    exercise 2 lesson 2 fruit shop

---
+++ modulo-2/modulo-2-leccion-2-ejercicio-3-fruteria/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+const kiwiKilo = 5;
+const peraKilo = 2;
+const uvaKilo = 4;
+
+const miCompra = kiwiKilo * 2 + peraKilo * 3 + uvaKilo / 2;
+console.log(miCompra);
+
+//intento de visualizar variable miCompra en HTML
+//const precioTotal = document.querySelector('.result');
+//precioTotal += precioTotal.innerHTML + 'miCompra';
