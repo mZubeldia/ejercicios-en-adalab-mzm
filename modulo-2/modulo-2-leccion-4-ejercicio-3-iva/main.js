573ceb3dbf292ffc3a2e7fbf06e4e919b5cb82c1
mZubeldia <mzubeldia09@gmail.com>
2021-05-05 19:01:45 +0200

    exercise iva

--- modulo-2/modulo-2-leccion-4-ejercicio-3-iva/main.js
+++ modulo-2/modulo-2-leccion-4-ejercicio-3-iva/main.js
@@ -1,2 +1,8 @@
 'use strict';
 
+function iva (a, b){
+    const ivaCalc = a * b;
+    return console.log(`El precio sin IVA es ${a}, el IVA es el ${b * 100}% y su total es ${ivaCalc}.`);
+}
+
+const ivaResult = iva(10, 0.21);
