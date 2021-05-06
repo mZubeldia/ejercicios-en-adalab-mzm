d01c7e3c5930a7c77e2beb302594ce1d4d5c7bbc
mZubeldia <mzubeldia09@gmail.com>
2021-05-06 18:58:46 +0200

    try exercise input name

---
+++ modulo-2/modulo-2-leccion-5-ejercicio-1-hola-input/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const button = document.querySelector('.js-btn');
+const nameInput = document.querySelector('.js-name');
+
+function hi () {
+nameInput = nameInput.value;
+
+
+
+
+//const consoleText = console.log `Hola ${userName}`;
+//return console.log `Hola, ${nameInput}`;
+}
+
+button.addEventListener('click', hi);
