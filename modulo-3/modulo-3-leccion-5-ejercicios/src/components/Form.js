b62d1ecec078ec9f3dc9385ca4d3d28f63bb419d
mZubeldia <mzubeldia09@gmail.com>
2021-06-05 12:28:03 +0200

    add exercise 1,2 lesson 3.1

---
+++ modulo-3/modulo-3-leccion-5-ejercicios/src/components/Form.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class Form extends React.Component {
+    constructor(props)
+    super(props)
+
+  render() {
+    return (
+        <form>
+            
+        </form>
+    );
+  }
+}
+
+
+
+
+
+export default Form;
