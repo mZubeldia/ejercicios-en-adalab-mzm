4b89229dacc431cab0d44da9bb98f6887bc00c48
mZubeldia <mzubeldia09@gmail.com>
2021-06-06 22:11:14 +0200

    add exercise 4, lesson 3

---
+++ modulo-3/modulo-3-leccion-3-ejercicio-4/src/components/Items.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class Items extends React.Component {
+  render() {
+    {
+      /* Pinto en el HTML los datos que me pasa el componente madre por PROPS */
+    }
+    return (
+      <>
+        <p>Nombre: {this.props.name}</p>
+        <p>Descripción: {this.props.description}</p>
+        <p>Cantidad: {this.props.quantity}</p>
+        <p>Categoría: {this.props.category}</p>
+        <p>Precio: {this.props.price}€</p>
+      </>
+    );
+  }
+}
+
+export default Items;
+
+Items.defaultProps = {
+  description: "No hay descripción.",
+};
+Items.propTypes = {
+  name: PropTypes.string.isRequired,
+  quantity: PropTypes.number.isRequired,
+};
