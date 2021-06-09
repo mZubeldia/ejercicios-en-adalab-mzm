a26991a9214881e420899894fb4cb3573fb65cb8
mZubeldia <mzubeldia09@gmail.com>
2021-06-09 20:14:49 +0200

    fix exercise 1 lesson 3.4

--- modulo-3/modulo-3-leccion-4-ejercicio-1/src/components/OnionHater.js
+++ modulo-3/modulo-3-leccion-4-ejercicio-1/src/components/OnionHater.js
@@ -1,26 +1,28 @@
-import React from 'react';
-
+import React from "react";
 
 class OnionHater extends React.Component {
-  render() 
-  {
-  const handleChange = (ev) => {
-    if (ev.target.value.includes("cebolla")) {
-      alert("ODIO LA CEBOLLA");
-    } else {
-        alert(ev.target.value + "no está mal")
+  render() {
+    const handleChange = (ev) => {
+      if (ev.target.value.includes("cebolla")) {
+        alert("ODIO LA CEBOLLA");
+      } else if (ev.target.value.includes("tomate")) {
+        alert("Me encanta el tomate");
+      }
     };
-  
-  
+
     return (
-      <label>
-      ¿Qué te gusta comer?
-      <input type="text" placeholder="Tomate" onChage={handleChange}></input>
-    </label>
+      <form>
+        <label>
+          ¿Qué te gusta comer?
+          <input
+            type="text"
+            placeholder="Tomate"
+            onChange={handleChange}
+          ></input>
+        </label>
+      </form>
     );
   }
 }
-
-
 
 export default OnionHater;
